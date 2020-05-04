c4e1570dfb0beb3734229697b1c2fc413d7aa102
alexmaruyama <alex.maruyama@gmail.com>
2020-05-03 21:25:50 -0300

    Projeto inicial

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:cara_coroa/jogo.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Jogo(),
+  ));
+}
